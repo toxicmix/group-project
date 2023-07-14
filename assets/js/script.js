2d455744c9d4e72ea6b9cc421e4d08804cd7fee4
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-07-14 12:16:09 -0500

    working on shazam call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
+var userInput = $('#root')
 
-
-fetch("https://shazam.p.rapidapi.com/auto-complete?term=${userinput}", {
+fetch(`https://shazam.p.rapidapi.com/auto-complete?term=${userInput}`, {
 "headers": {
 "x-rapidapi-key": "d2c16f9919mshf8833d93f140df5p1b038djsn746b6bea0db0",
 "x-rapidapi-host": "shazam.p.rapidapi.com"
@@ -14,3 +14,15 @@ return response.json ()
 .catch(err => {
 console.error(err);
 });
+$(function () {
+
+  
+  
+    $('.saveBtn').on("click", function() {
+      localStorage.setItem($(this).parent().attr('id'), $(this).siblings('.description').val());
+      console.log(localStorage.getItem($(this).parent().attr('id')));
+    }
+  );
+  
+ 
+  });
