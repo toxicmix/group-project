61daad471daff606e3b09e362625ab832c367438
Lauren Bleyl <laurenbleyl@gmail.com>
2023-07-14 14:48:43 -0500

    messed something up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 
 
 
-// fetch`https://shazam.p.rapidapi.com/auto-complete?term=${userinput}`, {
-// "headers": {
-// "x-rapidapi-key": "d2c16f9919mshf8833d93f140df5p1b038djsn746b6bea0db0",
-// "x-rapidapi-host": "shazam.p.rapidapi.com"
-// }
-// }
-// .then(response => {
-// return response.json ()
-// })
-// .then (data => {console.log(data)})
-// .catch(err => {
-// console.error(err);
-// });
+fetch`https://shazam.p.rapidapi.com/auto-complete?term=${userinput}`, {
+"headers": {
+"x-rapidapi-key": "d2c16f9919mshf8833d93f140df5p1b038djsn746b6bea0db0",
+"x-rapidapi-host": "shazam.p.rapidapi.com"
+}
+}
+.then(response => {
+return response.json ()
+})
+.then (data => {console.log(data)})
+.catch(err => {
+console.error(err);
+});
 
 
 
