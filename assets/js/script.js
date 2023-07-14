b8017b6968fe91d4730ff78dbe0c635d6c446ed3
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-07-14 14:40:22 -0500

    merged from main

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,25 +6,6 @@ let retString = localStorage.getItem("recent search")
 let retArray = JSON.parse(retString)
 console.log(retArray);
 
-
-
-
-// fetch`https://shazam.p.rapidapi.com/auto-complete?term=${userinput}`, {
-// "headers": {
-// "x-rapidapi-key": "d2c16f9919mshf8833d93f140df5p1b038djsn746b6bea0db0",
-// "x-rapidapi-host": "shazam.p.rapidapi.com"
-// }
-// }
-// .then(response => {
-// return response.json ()
-// })
-// .then (data => {console.log(data)})
-// .catch(err => {
-// console.error(err);
-// });
-
-
-
 $(function () {
     var userInput = "west coast"//($('#TextInput').val())
 
@@ -69,3 +50,16 @@ $(function () {
 //     console.error(err);
 //     });
 // }
+
+const fieldsetForm = document.querySelector('#fieldset')
+
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  const userinput = document.querySelector('#TextInput').value
+  console.log(userinput)
+  const queryString = './results.html?q='+userinput
+  location.assign(queryString)
+}
+
+fieldsetForm.addEventListener('submit', handleSearchFormSubmit)
